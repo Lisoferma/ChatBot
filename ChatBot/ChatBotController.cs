46ef983518e4a25855e48240fee529a72f523ba4
Lisoferma <pilipeiko.sasha@gmail.com>
2023-05-29 03:32:21 +0900

    Change directory structure

---
+++ ChatBot/ChatBotController.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LisofermaChatBot;
+
+public class ChatBotController : IBotResponse
+{
+    public IBotResponse CurrentBot { get; set; }
+
+    public delegate string InputHandler(string input);
+
+    private Dictionary<Regex, InputHandler> _dictInputHandlers;
+
+
+    public ChatBotController() : this(new ChatBotAIML()) { }
+
+
+    public ChatBotController(IBotResponse chatBot)
+    {       
+        CurrentBot = chatBot;
+        _dictInputHandlers = new Dictionary<Regex, InputHandler>();
+    }
+
+
+    public string GetAnswer(string input)
+    {
+        // Поиск обработчика ввода, если не найден - null.
+        InputHandler? foundHandler = 
+            _dictInputHandlers.FirstOrDefault(handler => handler.Key.IsMatch(input)).Value;
+
+        if (foundHandler != null)
+            return foundHandler(input);
+        else
+            return CurrentBot.GetAnswer(input);
+    }
+
+
+    public void AddInputHandler(Regex regex, InputHandler inputHandler)
+    {
+        _dictInputHandlers.Add(regex, inputHandler);
+    }
+}
+
---
+++ ChatBot/UserCommands.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+
+namespace LisofermaChatBot;
+
+public class UserCommands
+{
+    public static string GetTime(string input)
+    {
+        return $"{DateTime.Now:HH:mm}";
+    }
+
+
+    public static string CalculateExpressionFromString(string input)
+    {
+        DataTable table = new();
+
+        Convert.ToDouble(table.Compute(input, string.Empty));
+
+        input = input.Replace(" ", "");
+
+        // Убрать слово "умножь".
+        input = input.Substring(input.LastIndexOf('ь') + 1);
+
+        // Разбить строку на две подстроки.
+        string[] words =
+            input.Split(new char[] { 'н', 'а' }, StringSplitOptions.RemoveEmptyEntries);
+
+        try
+        {
+            int num1 = Convert.ToInt32(words[0]);
+            int num2 = Convert.ToInt32(words[1]);
+            return (num1 * num2).ToString();
+        }
+        catch
+        {
+            return "Извините, не могу разобрать. Повторите, пожалуйста, ввод.";
+        }
+    }
+}
