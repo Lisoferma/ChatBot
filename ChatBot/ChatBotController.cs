1b2f2fe3557b26c642b8b2bc528e50c7ba4a358d
Lisoferma <pilipeiko.sasha@gmail.com>
2023-05-30 00:37:19 +0900

    Add comments

--- ChatBot/ChatBotController.cs
+++ ChatBot/ChatBotController.cs
@@ -2,18 +2,38 @@
 
 namespace LisofermaChatBot;
 
+/// <summary>
+/// Контроллер для обработки сообщений пользователя.
+/// Содержит список команд для пользователя и текущий чатбот.
+/// </summary>
 public class ChatBotController : IBotResponse
 {
+    /// <summary>
+    /// Текущий чатбот от которого пользователь будет получать ответ.
+    /// </summary>
     public IBotResponse CurrentBot { get; set; }
 
+    /// <summary>
+    /// Делегат для методов обработки сообщений и команд пользователя.
+    /// </summary>
+    /// <param name="input">Сообщение от пользователя.</param>
+    /// <returns>Ответ на сообщение.</returns>
     public delegate string InputHandler(string input);
 
+    // Обработчики сообщений и regex который их вызывает.
     private Dictionary<Regex, InputHandler> _dictInputHandlers;
 
 
+    /// <summary>
+    /// Инициализирует текущий чатбот как ChatBotAIML.
+    /// </summary>
     public ChatBotController() : this(new ChatBotAIML()) { }
 
 
+    /// <summary>
+    /// Инициализировать и задать текущий чатбот. 
+    /// </summary>
+    /// <param name="chatBot">Чатбот который будет отвечать на сообщения.</param>
     public ChatBotController(IBotResponse chatBot)
     {       
         CurrentBot = chatBot;
@@ -21,6 +41,12 @@ public class ChatBotController : IBotResponse
     }
 
 
+    /// <summary>
+    /// Получить ответ на сообщение от подходящего обработчика
+    /// или, если его нет, получить ответ от чатбота.
+    /// </summary>
+    /// <param name="input">Сообщение от пользователя.</param>
+    /// <returns>Ответ на сообщение.</returns>
     public string GetAnswer(string input)
     {
         // Поиск подходящего обработчика ввода, если не найден - null.
@@ -34,6 +60,11 @@ public class ChatBotController : IBotResponse
     }
 
 
+    /// <summary>
+    /// Добавить обработчик сообщения пользователя.
+    /// </summary>
+    /// <param name="regex">Regex который будет вызывать обработчик.</param>
+    /// <param name="inputHandler">Обработчик сообщения.</param>
     public void AddInputHandler(Regex regex, InputHandler inputHandler)
     {
         _dictInputHandlers.Add(regex, inputHandler);
