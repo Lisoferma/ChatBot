1f3ac9fd873abe41b72d05a5603b43d9856a1e19
Lisoferma <pilipeiko.sasha@gmail.com>
2023-05-30 00:22:51 +0900

    Update logic

--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -1,11 +1,30 @@
-﻿namespace LisofermaChatBot;
+﻿using System.Text.RegularExpressions;
+using static LisofermaChatBot.UserCommands;
+
+namespace LisofermaChatBot;
 
 internal class Program
 {
     static void Main()
     {
-        IBotResponse chatbot = new ChatBotAIML();
+        ChatBotController chatbot = new();
         string? input;
+
+        chatbot.AddInputHandler(
+            new Regex(@"(time)|(время)|(который час)|(который час)"),
+            GetTime);
+
+        chatbot.AddInputHandler(
+            new Regex(@"(date)|(дата)|(какая сегодня дата)"),
+            GetDate);
+
+        chatbot.AddInputHandler(
+            new Regex(@"(calculate)|(compute)|(вычисли)|(посчитай)"),
+            CalculateExpressionFromString);
+
+        chatbot.AddInputHandler(
+            new Regex(@"(currency rate)|(курс валют)|(курс)"),
+            GetCurrencyRate);
 
         do
         {
