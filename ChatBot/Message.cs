b7cb688e721d8e60a0e2a4e350524f29446a946f
Lisoferma <pilipeiko.sasha@gmail.com>
2023-05-30 10:40:51 +0900

    Create Message class

---
+++ ChatBot/Message.cs
@@ -0,0 +1,35 @@
+﻿namespace ChatBot;
+
+public class Message
+{
+    /// <summary>
+    /// Текст сообщения.
+    /// </summary>
+    public string Text { get; set; }
+
+    /// <summary>
+    /// Кем написано сообщение.
+    /// </summary>
+    public string Author
+    {
+        get => Text;
+
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException(nameof(Author), "Value cannot be null or empty");
+
+            Text = value;
+        }      
+    }
+
+
+    /// <summary>
+    /// Инициализирует сообщение пустым текстом и Undefined автором.
+    /// </summary>
+    public Message()
+    {
+        Text = string.Empty;
+        Author = "Undefined";
+    }
+}
