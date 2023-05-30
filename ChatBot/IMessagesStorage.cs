91a938eed93776545f6533ef5bbc9f4895439adf
Lisoferma <pilipeiko.sasha@gmail.com>
2023-05-30 12:28:22 +0900

    Add IMessageStorage

---
+++ ChatBot/IMessagesStorage.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ChatBot;
+
+public interface IMessagesStorage
+{
+    void Save(ObservableCollection<Message> messages);
+
+    void Load(ObservableCollection<Message> messages);
+}
