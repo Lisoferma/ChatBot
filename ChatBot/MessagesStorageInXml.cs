13fee61b2254af4fde41d61a9ff8fc39917d8845
Lisoferma <pilipeiko.sasha@gmail.com>
2023-05-30 12:29:27 +0900

    Add MessagesStorageInXml

---
+++ ChatBot/MessagesStorageInXml.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ChatBot;
+
+public class MessagesStorageInXml : IMessagesStorage
+{
+    public void Save(ObservableCollection<Message> messages)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public void Load(ObservableCollection<Message> messages)
+    {
+        throw new NotImplementedException();
+    }
+}
