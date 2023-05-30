7be50adbf189c1ecf12432f59175c153276a8e4a
Lisoferma <pilipeiko.sasha@gmail.com>
2023-05-30 11:14:59 +0900

    Add TimeStamp and constructors

--- ChatBot/Message.cs
+++ ChatBot/Message.cs
@@ -1,5 +1,8 @@
 ﻿namespace ChatBot;
 
+/// <summary>
+/// Сообщение, содержит текст и его автора.
+/// </summary>
 public class Message
 {
     /// <summary>
@@ -23,13 +26,48 @@ public class Message
         }      
     }
 
+    /// <summary>
+    /// Время отправки.
+    /// </summary>
+    public DateTime TimeStamp { get; set; }
+
 
     /// <summary>
-    /// Инициализирует сообщение пустым текстом и Undefined автором.
+    /// Инициализирует сообщение пустым текстом, Undefined автором
+    /// и текущим временем.
     /// </summary>
     public Message()
     {
         Text = string.Empty;
         Author = "Undefined";
+        TimeStamp = DateTime.Now;
+    }
+
+
+    /// <summary>
+    /// Инициализирует сообщение заданными свойствами.
+    /// </summary>
+    /// <param name="text">Текст сообщения.</param>
+    /// <param name="author">От кого сообщение.</param>
+    /// <param name="timeStamp">Время отправки.</param>
+    public Message(string text, string author, DateTime timeStamp)
+    {
+        Text = text;
+        Author = author;
+        TimeStamp = timeStamp;
+    }
+
+
+    /// <summary>
+    /// Инициализирует сообщение заданными свойствами
+    /// и текущим временем.
+    /// </summary>
+    /// <param name="text">Текст сообщения.</param>
+    /// <param name="author">От кого сообщение.</param>
+    public Message(string text, string author)
+    {
+        Text = text;
+        Author = author;
+        TimeStamp = DateTime.Now;
     }
 }
