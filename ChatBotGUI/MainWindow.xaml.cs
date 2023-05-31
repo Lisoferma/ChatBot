cdd112e8457a78648adaa722412b388eed5c9712
Lisoferma <pilipeiko.sasha@gmail.com>
2023-05-31 15:00:55 +0900

    Add handlers and window logic

--- ChatBotGUI/MainWindow.xaml.cs
+++ ChatBotGUI/MainWindow.xaml.cs
@@ -1,28 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ChatBot;
+using static ChatBot.UserCommands;
 
-namespace ChatBotGUIExample
+namespace ChatBotGUI;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public string Username { get; set; }
+    public const string BotName = "Bot";
+
+    private List<Message> chatHistory;
+    private readonly ChatBotController chatbot;
+
+
+    public MainWindow()
     {
-        public MainWindow()
+        DataContext = this;
+        Username = "User";
+        chatHistory = new List<Message>();
+        chatbot = new ChatBotController();
+
+        InitializeInputHandler();
+        InitializeComponent();           
+    }
+
+
+    private void Button_send_Click(object sender, RoutedEventArgs e)
+    {
+        GetChatbotAnswer();
+    }
+
+
+    private void GetChatbotAnswer()
+    {
+        string input = TextBox_input.Text;
+        TextBox_input.Text = string.Empty;
+
+        if (string.IsNullOrEmpty(input)) return;
+
+        Message userMessage = new()
         {
-            InitializeComponent();
+            Text = input,
+            Author = Username,
+            TimeStamp = DateTime.Now
+        };
+
+        TextBlock_chat.Text += $"{Username}: {input}\n";
+        chatHistory.Add(userMessage);
+
+        string answer = chatbot.GetAnswer(input);
+
+        Message botMessage = new()
+        {
+            Text = answer,
+            Author = BotName,
+            TimeStamp = DateTime.Now
+        };
+
+        TextBlock_chat.Text += $"{BotName}: {answer}\n";
+        ScrollViewer_chatScroll.ScrollToEnd();
+        chatHistory.Add(botMessage);
+    }
+
+
+    private void InitializeInputHandler()
+    {
+        chatbot.AddInputHandler(
+            new Regex(@"(time)|(время)|(который час)|(который час)"),
+            GetTime);
+
+        chatbot.AddInputHandler(
+            new Regex(@"(date)|(дата)|(какая сегодня дата)"),
+            GetDate);
+
+        chatbot.AddInputHandler(
+            new Regex(@"(calculate)|(compute)|(вычисли)|(посчитай)"),
+            CalculateExpressionFromString);
+
+        chatbot.AddInputHandler(
+            new Regex(@"(currency rate)|(курс валют)|(курс)"),
+            GetCurrencyRate);
+    }
+
+
+    protected override void OnClosing(CancelEventArgs e)
+    {
+        string path = "messages.xml";
+        string xml =
+            ObjectSerializerToXml< List<Message> >.ToXml(chatHistory);
+
+        using (StreamWriter writer = new(path, false))
+        {
+            writer.WriteLine(xml);
         }
     }
 }
