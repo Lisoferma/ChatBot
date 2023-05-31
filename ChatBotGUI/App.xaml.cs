d43806d528a4e8ff753516029cae3c8b2b442c4a
Lisoferma <pilipeiko.sasha@gmail.com>
2023-05-31 14:59:04 +0900

    Rename namespace

--- ChatBotGUI/App.xaml.cs
+++ ChatBotGUI/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ChatBotGUIExample
+namespace ChatBotGUI
 {
     /// <summary>
     /// Interaction logic for App.xaml
