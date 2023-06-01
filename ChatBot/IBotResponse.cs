231091c22f8383106cc0440e2ed56164f8536ab5
Lisoferma <pilipeiko.sasha@gmail.com>
2023-06-01 20:32:19 +0900

    Add comments

--- ChatBot/IBotResponse.cs
+++ ChatBot/IBotResponse.cs
@@ -1,7 +1,15 @@
 ﻿namespace ChatBot;
 
+/// <summary>
+/// Представляет метод для получения ответа от чатбота на основе переданной строки.
+/// </summary>
 public interface IBotResponse
 {
+    /// <summary>
+    /// Получить ответ от бота, на основе переданной строки.
+    /// </summary>
+    /// <param name="input">Строка которую будет обрабатывать бот.</param>
+    /// <returns>Ответ от бота.</returns>
     string GetAnswer(string input);
 }
 
