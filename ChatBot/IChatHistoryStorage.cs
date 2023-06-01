a1554a8111117e7969f1d3e35563018a8b5cf816
Lisoferma <pilipeiko.sasha@gmail.com>
2023-06-01 20:32:57 +0900

    New implementation of methods

--- ChatBot/IChatHistoryStorage.cs
+++ ChatBot/IChatHistoryStorage.cs
@@ -1,8 +1,39 @@
-﻿namespace ChatBot;
+﻿using System.Collections.ObjectModel;
 
+namespace ChatBot;
+
+/// <summary>
+/// Предоставляет методы для сохранения и загрузки <see cref="ObservableCollection<Message>"/>.
+/// </summary>
 public interface IChatHistoryStorage
 {
-    void Save(ICollection<Message> messages);
+    /// <summary>
+    /// Путь к файлу сохранения.
+    /// </summary>
+    public string Filepath { get; set; }
 
-    void Load(ICollection<Message> messages);
+
+    /// <summary>
+    /// Сохраненить <see cref="ObservableCollection<Message>"/>;
+    /// путь к файлу - <see cref="Filepath"/>.
+    /// </summary>
+    /// <param name="chatHistory">История чата которую нужно сохранить.</param>
+    void Save(ObservableCollection<Message> chatHistory);
+
+
+    /// <summary>
+    /// Сохраненить <see cref="ObservableCollection<Message>"/>. 
+    /// </summary>
+    /// <param name="chatHistory">История чата которую нужно сохранить.</param>
+    /// <param name="filepath">Путь к файлу сохранения.</param>
+    /// <exception cref="ArgumentNullException"></exception>
+    void SaveAs(ObservableCollection<Message> chatHistory, string filepath);
+
+
+    /// <summary>
+    /// Загрузить историю чата из файла.
+    /// </summary>
+    /// <param name="filepath">Файл из которого нужно загрузить.</param>
+    /// <returns>История чата.</returns>
+    ObservableCollection<Message>? LoadFrom(string filepath);
 }
